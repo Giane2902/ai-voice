baa9fd7ec7b5f1d10bbbb32a8bfbd9f3b34316f9
Giane2902 <gianella2902@gmail.com>
2022-12-05 18:34:12 -0500

    ADD: Jarvis.py init

---
+++ jarvis.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+engine.say("Buenos días")
+engine.runAndWait()
+
+try:
+    with sr.Microphone() as source:
+        print("Listening...")
+        voice = listener.listen(source)
+        rec = listener.recognize_google(voice)
+        print(rec)
+except:
+    pass
